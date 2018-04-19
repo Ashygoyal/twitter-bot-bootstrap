bbe4205cd9422ae0ffddc34d2016b75c7db52657
ashygoyal <goyal.ashish3@gmail.com>
2018-04-19 23:48:09 +0530

    Making twitter-bot-bootstrap a non git folder

---
+++ my-node-workspace/workspace/twitter-bot-bootstrap/src/config.js
@@ -0,0 +1,27 @@
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+
+// Uncomment for when deploying to Heroku
+
+ module.exports = {
+   consumer_key: process.env.CONSUMER_KEY,
+   consumer_secret: process.env.CONSUMER_SECRET,
+   access_token: process.env.ACCESS_TOKEN,
+   access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+ };
+
+require('dotenv').config(); 
+
+module.exports = {
+  consumer_key: 'BYT4tdnT5ITxRY3keX7CNwvhG',
+  consumer_secret: 'd2kf2lzq4nXEikpsQepsBBBuiTsxD4Tn6iIbScYKoVOJGrFuWm',
+  access_token: '147808626-z6JMuIgG6ErBEuId0nNuESXYIINz0GsAO1CpSHYV',
+  access_token_secret: 'foT8Z77X4hXvMw20xRiOrXS5tVc5LkH6ctpVQceBpOzZx'
+}
+
+
+
